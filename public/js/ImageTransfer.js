6ec915d8e7268ab85061e5b9fa88423f144d8ba0
中島浩二 <knaka0209@gmail.com>
2020-12-26 15:47:09 +0900

    up,1226a4

--- public/js/ImageTransfer.js
+++ public/js/ImageTransfer.js
@@ -7,6 +7,21 @@ var ImageTransfer = {
         if (typeof fileObject === "undefined") {
             return;
         }
+        //
+        console.log("Name: " + fileObject.name);
+        console.log("Size: " + fileObject.size);
+        console.log("Type: " + fileObject.type);
+        console.log("Date: " + fileObject.lastModified);
+        console.log("Date: " + fileObject.lastModifiedDate);
+        if (fileObject.type.match(/^image\/(jpeg|png)$/) === null) {
+            alert("Error, 画像ファイル種類はjpeg/png以外は登録できません")
+            return;
+        }
+        var max_fileSize = 1000 * 1000;
+        if(fileObject.size > max_fileSize){
+            alert("Error, ファイルサイズ最大値を超えました。")
+            return;
+        }
         var formData = new FormData();
         formData.append(id_file, fileObject );
         var xhr = new XMLHttpRequest();
